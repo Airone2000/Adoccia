321078851c6ad48152ec26ef4f44a40d4ff4354c
Erwan GUILLOU <r.wan.guillou+github@gmail.com>
2019-10-18 10:55:45 +0200

    Use Encore to manage asset

--- assets/js/form/edit.js
+++ assets/js/form/edit.js
@@ -1,9 +1,65 @@
 require('./../../css/form/edit.scss');
+import $ from './../../vendor/jquery/jquery';
+import Sortable from './../../vendor/Sortable/Sortable';
 
 class Edit
 {
     constructor() {
-        console.log('coucou');
+        this._makeItSortable();
+        this._listenForResizeArea();
+        this._listenForAddArea();
+        this._listenForDeleteArea();
+    }
+
+    _makeItSortable() {
+        let el = document.getElementById('form');
+        new Sortable(el, {
+            animation: 150,
+            chosenClass: "sortable-chosen",
+            dragClass: "sortable-drag",
+            easing: "cubic-bezier(1, 0, 0, 1)",
+            handle: '.handle',
+            draggable: '.area'
+        });
+    }
+
+    _listenForResizeArea() {
+        $(document).on('change', '.resizer', function(){
+            let $resizer = $(this);
+            const selectedSize = +$resizer.val();
+            const maxSize = +$resizer.prop('max');
+            const minSize = +$resizer.prop('min');
+
+            let size = selectedSize;
+
+            size = size < minSize ? minSize : size;
+            size = size > maxSize ? maxSize : size;
+            $resizer.val(size);
+            $resizer.parents('.area').css('width', 'calc('+size+'% - 20px)');
+        });
+    }
+
+    _listenForAddArea() {
+        $('.add-formArea').on('click', function(){
+            let insertType = $(this).data('inserttype');
+            let $newElement = $($('#sampleFormArea').html());
+            if (insertType === 'prepend') {
+                $('#form').prepend($newElement);
+            }
+            else {
+                $('#form').append($newElement);
+            }
+        });
+    }
+
+    _listenForDeleteArea() {
+        $(document).on('click', '.delete-formArea', function(){
+            let $area = $(this).parents('.area');
+            $area.css('opacity', 0);
+            setTimeout(function(){
+                $area.remove();
+            }, 200);
+        });
     }
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,7 @@ Encore
      * Each entry will result in one JavaScript file (e.g. app.js)
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
-    .addEntry('app', './assets/js/app.js')
+    .addStyleEntry('theme', './assets/css/theme.scss')
     .addEntry('form/edit', './assets/js/form/edit.js')
     //.addEntry('page1', './assets/js/page1.js')
     //.addEntry('page2', './assets/js/page2.js')
