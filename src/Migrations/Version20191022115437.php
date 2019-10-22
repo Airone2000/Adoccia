4a231ec8d8a4d6c929eb1f731f3a66814e205ee6
Erwan GUILLOU <r.wan.guillou+github@gmail.com>
2019-10-22 13:54:48 +0200

    Migrations

--- src/Migrations/Version20191022115437.php
+++ src/Migrations/Version20191022115437.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20191019202059 extends AbstractMigration
+final class Version20191022115437 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -22,17 +22,18 @@ final class Version20191019202059 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('CREATE TABLE category (id INT AUTO_INCREMENT NOT NULL, created_by_id INT DEFAULT NULL, picture_id INT DEFAULT NULL, name VARCHAR(255) NOT NULL, description LONGTEXT DEFAULT NULL, created_at DATETIME NOT NULL, published TINYINT(1) DEFAULT \'0\' NOT NULL, INDEX IDX_64C19C1B03A8386 (created_by_id), UNIQUE INDEX UNIQ_64C19C1EE45BDBF (picture_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE category (id INT AUTO_INCREMENT NOT NULL, created_by_id INT DEFAULT NULL, picture_id INT DEFAULT NULL, form_id INT DEFAULT NULL, draft_form_id INT DEFAULT NULL, name VARCHAR(255) NOT NULL, description LONGTEXT DEFAULT NULL, created_at DATETIME NOT NULL, published TINYINT(1) DEFAULT \'0\' NOT NULL, INDEX IDX_64C19C1B03A8386 (created_by_id), UNIQUE INDEX UNIQ_64C19C1EE45BDBF (picture_id), UNIQUE INDEX UNIQ_64C19C15FF69B7D (form_id), UNIQUE INDEX UNIQ_64C19C1A73FE35E (draft_form_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
         $this->addSql('CREATE TABLE password_reset (id INT AUTO_INCREMENT NOT NULL, user_id INT DEFAULT NULL, cached_email VARCHAR(255) NOT NULL, created_at DATETIME NOT NULL, expires_at DATETIME NOT NULL, password_changed_at DATETIME DEFAULT NULL, token VARCHAR(70) NOT NULL, UNIQUE INDEX UNIQ_B10172525F37A13B (token), INDEX IDX_B1017252A76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
         $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, username VARCHAR(180) NOT NULL, roles JSON NOT NULL, email VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL, permissions JSON NOT NULL, is_super_admin TINYINT(1) DEFAULT \'0\' NOT NULL, UNIQUE INDEX UNIQ_8D93D649F85E0677 (username), UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE form (id INT AUTO_INCREMENT NOT NULL, category_id INT NOT NULL, is_in_maintenance_mode TINYINT(1) NOT NULL, UNIQUE INDEX UNIQ_5288FD4F12469DE2 (category_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE widget (id INT AUTO_INCREMENT NOT NULL, form_area_id INT NOT NULL, type VARCHAR(30) NOT NULL, UNIQUE INDEX UNIQ_85F91ED08C6320A7 (form_area_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE form (id INT AUTO_INCREMENT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE widget (id INT AUTO_INCREMENT NOT NULL, form_area_id INT NOT NULL, type VARCHAR(30) NOT NULL, inner_text LONGTEXT DEFAULT NULL, min_length BIGINT DEFAULT NULL, max_length BIGINT DEFAULT NULL, required TINYINT(1) DEFAULT \'0\' NOT NULL, UNIQUE INDEX UNIQ_85F91ED08C6320A7 (form_area_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
         $this->addSql('CREATE TABLE picture (id INT AUTO_INCREMENT NOT NULL, category_id INT DEFAULT NULL, user_id INT DEFAULT NULL, filename VARCHAR(255) NOT NULL, public SMALLINT NOT NULL, updated_at DATETIME DEFAULT NULL, UNIQUE INDEX UNIQ_16DB4F8912469DE2 (category_id), INDEX IDX_16DB4F89A76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE form_area (id INT AUTO_INCREMENT NOT NULL, form_id INT NOT NULL, widget_id INT NOT NULL, width DOUBLE PRECISION DEFAULT \'100\' NOT NULL, position INT NOT NULL, INDEX IDX_43B5397F5FF69B7D (form_id), UNIQUE INDEX UNIQ_43B5397FFBE885E2 (widget_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE form_area (id INT AUTO_INCREMENT NOT NULL, form_id INT NOT NULL, widget_id INT DEFAULT NULL, width DOUBLE PRECISION DEFAULT \'100\' NOT NULL, position INT NOT NULL, INDEX IDX_43B5397F5FF69B7D (form_id), UNIQUE INDEX UNIQ_43B5397FFBE885E2 (widget_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
         $this->addSql('ALTER TABLE category ADD CONSTRAINT FK_64C19C1B03A8386 FOREIGN KEY (created_by_id) REFERENCES user (id)');
         $this->addSql('ALTER TABLE category ADD CONSTRAINT FK_64C19C1EE45BDBF FOREIGN KEY (picture_id) REFERENCES picture (id) ON DELETE SET NULL');
+        $this->addSql('ALTER TABLE category ADD CONSTRAINT FK_64C19C15FF69B7D FOREIGN KEY (form_id) REFERENCES form (id) ON DELETE SET NULL');
+        $this->addSql('ALTER TABLE category ADD CONSTRAINT FK_64C19C1A73FE35E FOREIGN KEY (draft_form_id) REFERENCES form (id) ON DELETE SET NULL');
         $this->addSql('ALTER TABLE password_reset ADD CONSTRAINT FK_B1017252A76ED395 FOREIGN KEY (user_id) REFERENCES user (id) ON DELETE CASCADE');
-        $this->addSql('ALTER TABLE form ADD CONSTRAINT FK_5288FD4F12469DE2 FOREIGN KEY (category_id) REFERENCES category (id) ON DELETE CASCADE');
         $this->addSql('ALTER TABLE widget ADD CONSTRAINT FK_85F91ED08C6320A7 FOREIGN KEY (form_area_id) REFERENCES form_area (id) ON DELETE CASCADE');
         $this->addSql('ALTER TABLE picture ADD CONSTRAINT FK_16DB4F8912469DE2 FOREIGN KEY (category_id) REFERENCES category (id) ON DELETE SET NULL');
         $this->addSql('ALTER TABLE picture ADD CONSTRAINT FK_16DB4F89A76ED395 FOREIGN KEY (user_id) REFERENCES user (id) ON DELETE SET NULL');
@@ -45,11 +46,12 @@ final class Version20191019202059 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE form DROP FOREIGN KEY FK_5288FD4F12469DE2');
         $this->addSql('ALTER TABLE picture DROP FOREIGN KEY FK_16DB4F8912469DE2');
         $this->addSql('ALTER TABLE category DROP FOREIGN KEY FK_64C19C1B03A8386');
         $this->addSql('ALTER TABLE password_reset DROP FOREIGN KEY FK_B1017252A76ED395');
         $this->addSql('ALTER TABLE picture DROP FOREIGN KEY FK_16DB4F89A76ED395');
+        $this->addSql('ALTER TABLE category DROP FOREIGN KEY FK_64C19C15FF69B7D');
+        $this->addSql('ALTER TABLE category DROP FOREIGN KEY FK_64C19C1A73FE35E');
         $this->addSql('ALTER TABLE form_area DROP FOREIGN KEY FK_43B5397F5FF69B7D');
         $this->addSql('ALTER TABLE form_area DROP FOREIGN KEY FK_43B5397FFBE885E2');
         $this->addSql('ALTER TABLE category DROP FOREIGN KEY FK_64C19C1EE45BDBF');
